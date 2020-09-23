7a75bf84d49f6b7c6625a423c3d46307c43f9f08
ckn00b <cknew.dev@gmail.com>
2020-09-23 16:59:34 -0400

    rendering html and css with react

--- react/Ex_Files_React_js_EssT/ExcerciseFiles/Ch02/02_02/start/react-is-fun/src/index.js
+++ react/Ex_Files_React_js_EssT/ExcerciseFiles/Ch02/02_02/start/react-is-fun/src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+var style = {
+  backgroundColor: 'orange',
+  color: 'white',
+  fontFamily: 'Arial'
+}
+
+// const title = react.createElement(
+//   'ul',
+//   { id: 'title', className: 'header', style: style },
+//   react.createElement(
+//     'li',
+//     {},
+//     'item on our list'
+//   )
+// )
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  /* title */<div style={style}>
+    <h1 id='heading-element'>Hello World!</h1>
+    <p>I'm glad you're here!</p>
+  </div>,
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
